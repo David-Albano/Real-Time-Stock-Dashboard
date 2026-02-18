7bfe2fc8ff4436fbb6d8c30f3a2585ca8e2a5c8d
David-Albano <afonso27000@gmail.com>
2026-02-18 17:09:04 +0000

    Update auth.interceptor.ts

--- client-trading/src/app/auth/auth.interceptor.ts
+++ client-trading/src/app/auth/auth.interceptor.ts
@@ -1,12 +1,6 @@
 import { Injectable } from '@angular/core';
-import {
-  HttpInterceptor,
-  HttpRequest,
-  HttpHandler,
-  HttpEvent,
-  HttpErrorResponse
-} from '@angular/common/http';
-import { Observable, throwError, BehaviorSubject, filter, take, switchMap, catchError, retry } from 'rxjs';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError, BehaviorSubject, filter, take, switchMap, catchError } from 'rxjs';
 import { AuthService } from './auth.service';
 import { HttpClient } from '@angular/common/http';
 
@@ -58,6 +52,7 @@ export class AuthInterceptor implements HttpInterceptor {
 
           ).pipe(
               switchMap(() => {
+                console.log('inside switchMap now ««««««««')
                 this.isRefreshing = false;
                 this.refreshSubject.next(true);
                 
@@ -66,10 +61,13 @@ export class AuthInterceptor implements HttpInterceptor {
               }),
 
               catchError(error => {
+                console.log('error inside handle401Error =>: ', error)
                 this.isRefreshing = false;
                 this.authServiceAux.logout().subscribe(res => {
                   console.log('\n LOGGING OUT FROM AUTH.INTERCEPTOR.TS: ',res)
                 });
+
+                this.authServiceAux.clearSession();
                 
                 console.log('going here throwing error 222222222:', error)
                 return throwError(() => error);
@@ -78,81 +76,18 @@ export class AuthInterceptor implements HttpInterceptor {
             )
         };
 
+        console.log('going hereeee somewhere <<<<<<<<<<')
+
         return this.refreshSubject.pipe(
           filter(success => success === true),
           take(1),
           switchMap(() => {
             return next.handle(req.clone({withCredentials: true}));
+          }),
+          catchError((error) => {
+            console.log('err AFTER EVERYTHING: ', error)
+            return throwError(() => error); 
           })
         )
     }
 }
-
-
-
-// ================================
-// ================================
-// ================================
-
-
-
-// import { Injectable } from '@angular/core';
-// import {
-//   HttpInterceptor,
-//   HttpRequest,
-//   HttpHandler,
-//   HttpEvent,
-//   HttpErrorResponse
-// } from '@angular/common/http';
-// import { Observable, throwError, switchMap, catchError } from 'rxjs';
-// import { AuthService } from './auth.service';
-// import { HttpClient } from '@angular/common/http';
-
-// @Injectable()
-// export class AuthInterceptor implements HttpInterceptor {
-
-//   private url = 'http://localhost:8000/auth';
-//   private isRefreshing = false;
-
-//   constructor(private authServiceAux: AuthService, private http: HttpClient) {}
-
-//   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-
-//     // always send cookies
-//     const cloned = req.clone({
-//       withCredentials: true
-//     });
-
-//     // console.log('\n\n Intercepting Request: +++++++', cloned)
-
-//     return next.handle(cloned).pipe(
-
-//       catchError((error: HttpErrorResponse) => {
-
-//         if (error.status === 401 && !this.isRefreshing) {
-//           this.isRefreshing = true;
-
-//           return this.http.post(
-//                 `${this.url}/refresh`,
-//                 {},
-//                 { withCredentials: true }
-//             ).pipe(
-//                 switchMap(() => {
-//                     this.isRefreshing = false;
-
-//                     // retry original request
-//                     return next.handle(req.clone({ withCredentials: true }));
-//                 }),
-//                 catchError(err => {
-//                     this.isRefreshing = false;
-//                     this.authServiceAux.logout();
-//                     return throwError(() => err);
-//                 })
-//             );
-//         }
-
-//         return throwError(() => error);
-//       })
-//     );
-//   }
-// }
