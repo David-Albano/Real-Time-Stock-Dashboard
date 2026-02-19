b95cfc02fbe3f284532734a365c51550362a5be5
David-Albano <afonso27000@gmail.com>
2026-02-19 12:16:08 +0000

    Update app.routes.ts

--- client-trading/src/app/app.routes.ts
+++ client-trading/src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { authGuard } from './auth/auth.guard';
 import { PublicLayout } from './layouts/PublicLayout/PublicLayout';
 import { MainLayout } from './layouts/MainLayout/MainLayout';
 import { Home } from './layouts/MainLayout/home/home';
+import { News } from './features/news/news';
 
 export const routes: Routes = [
     // public routes
@@ -26,6 +27,7 @@ export const routes: Routes = [
             { path: '', redirectTo: 'home', pathMatch: 'full' },
             { path: 'home', component: Home },
             { path: 'dashboard', component: Dashboard },
+            { path: 'news', component: News },
         ]
     },
 
